46460fe8431db220b09d9d180eb005200b4c6eea
Douglas Simões <Douglass.vieira2014@gmail.com>
2025-06-04 19:28:50 -0300

    Teste de nova tela home

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import ListaPessoas from './components/admin/ListaClientes';
 import Formulario from './components/cliente/Formulario';
 import DetalhePessoa from './components/cliente/PainelCliente';
 import EditarPessoa from './components/admin/EditarCliente';
+import Teste from './components/cliente/Teste';
 
 // Assumindo que os arquivos dos componentes estão em src/components/
 // e exportam default ou o nome correspondente.
@@ -16,7 +17,7 @@ function App() {
       {/* Você pode adicionar um Navbar ou Layout aqui, se desejar, fora do <Routes> */}
       <div className="app-container"> {/* Exemplo de um container geral */}
         <Routes>
-          <Route path="/" element={<ListaPessoas />} />
+          <Route path="/" element={<Teste />} />
           <Route path="/cadastro" element={<Formulario />} />
           <Route path="/pessoa/:id" element={<DetalhePessoa />} />
           <Route path="/editar/:id" element={<EditarPessoa />} />
---
+++ client/src/components/cliente/Teste.js
@@ -0,0 +1,9 @@
+function Teste(){
+    return(
+        <div>
+            <h1>Bem Vindo ao InTime</h1>
+        </div>
+    );
+}
+
+export default Teste;
