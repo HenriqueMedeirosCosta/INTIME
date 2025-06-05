f03f1e10921007cf570541b5b763b28279720ea2
HenriqueMedeirosCosta <henriquemedeiros101@gmail.com>
2025-06-05 09:04:03 -0300

    Removendo a chave de segurança

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const clientesRoutes = require('./routes/clientesRoutes');
 
 const app = express();
